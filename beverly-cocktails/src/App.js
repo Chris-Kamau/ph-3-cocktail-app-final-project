ac3d12c9f2377f52d66b08eb23ca0b86ff2c9567
Chris <chris.kamau@student.moringaschool.com>
2023-06-07 22:02:10 +0300

    Add Navbar

--- beverly-cocktails/src/App.js
+++ beverly-cocktails/src/App.js
@@ -2,25 +2,18 @@ import React from 'react';
 import DisplayCocktail from './components/DisplayCocktails';
 import './App.css';
 import Navbar from './components/Navbar';
-// import LiquorTable from './components/TableLiquor';
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import AddLiquorForm from './components/AddLiquorForm';
-// import FeedbackForm from './components/FeedbackForm';
-// import Footer from './components/Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div>
     <BrowserRouter>
-  <NavBar />
+  <Navbar />
   <Routes>
   <Route path="/" element={ <DisplayCocktail />}>
   </Route>
   <Route path="/products" element={ <DisplayCocktail />}>
   </Route>
-  {/* <Route path="/table" element={ <LiquorTable />}>
-  </Route>
-  <Route path="/form" element={ <AddLiquorForm />}>
-  </Route> */}
   </Routes>
   </BrowserRouter>
   </div>
--- beverly-cocktails/src/components/Navbar.js
+++ beverly-cocktails/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { NavLink } from "react-router-dom";
-import './NavBar.css';
+import './Navbar.css';
 
 function NavBar() {
   const [searchQuery, setSearchQuery] = useState('');
